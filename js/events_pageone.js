12eb14282095d9efefcf29cc413872184d866e00
andersonp <Ande@1995>
2019-11-12 09:19:20 -0300

    first commit

---
+++ js/events_pageone.js
@@ -0,0 +1,142 @@
+function more() {
+  location.href = "more.html";
+}
+function changevideo02() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/i_v1qv4ra0M");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Conheça os setores da ABBR";
+}
+
+function changevideo03() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/qj67azDWllk");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML =
+    "Setor de Terapia Ocupacional Geral";
+}
+
+function changevideo04() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/Y3aF-VOar2k");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML =
+    "Setor de Reabilitação Respiratória";
+}
+
+function changevideo05() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/b85Acd-QKMk");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Medicina Esportiva";
+}
+
+function changevideo06() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/pgydP16gbSE");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML =
+    "Setor de Terapia Ocupacional Infantil";
+}
+
+function changevideo07() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/gY2Qzj2EXqs");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Video Institucional ABBR";
+}
+
+function changevideo08() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/Lc_R8EUiUmM");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML =
+    "Setor de Atividade da Vida Diaria - AVD";
+}
+
+function changevideo09() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/yH2uScPC3Ko");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML =
+    "Setor de Pilates Fisioterapêutico";
+}
+
+function changevideo10() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/xZbnfDa79ZA");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Oficina Ortopédica";
+}
---
+++ js/events_two.js
@@ -0,0 +1,122 @@
+function back() {
+  location.href = "index.html";
+}
+function changevideo02() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/Tw9Bbw0wdbI");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Por dentro da ABBR";
+}
+
+function changevideo03() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/-vFurF6wjwc");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Fonoaudiologia";
+}
+
+function changevideo04() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/USMSB7vYoB4");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Psicologia";
+}
+
+function changevideo05() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/OD5wXp8qGMI");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Pódio ABBR - Ano Olímpico";
+}
+
+function changevideo06() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/CwLgD9hpDug");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Serviço Social";
+}
+
+function changevideo07() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/PQZrCz49KhQ");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Estimulação Pedagógica";
+}
+
+function changevideo08() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/vy8Vyz1_yfs");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Setor de Hidroterapia";
+}
+
+function changevideo09() {
+  divMain = document.getElementById("main-header");
+  document.getElementById("temp").remove();
+  div = document.createElement("div");
+  div.setAttribute("id", "temp");
+  frm = document.createElement("IFRAME");
+  frm.setAttribute("src", "https://www.youtube.com/embed/GGPgW7_k8Pc");
+  frm.setAttribute("frameborder", "0");
+  frm.setAttribute("allowfullscreen", "");
+  frm.setAttribute("class", "thumb-main");
+  div.appendChild(frm);
+  divMain.appendChild(div);
+  document.getElementById("txt1").innerHTML = "Marcio Garcia fala da ABBR";
+}
